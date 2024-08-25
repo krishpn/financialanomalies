e69cc864349d079983a636dc739ec5a6df1c8c3d
Krishna Neupane <dataneupane@gmail.com>
2024-08-25 06:40:52 -0400

    math added

--- commitRender.sh
+++ commitRender.sh
@@ -2,7 +2,7 @@
 # Ask the user for their name
 
 rm *pdf
-quarto preview ./index.qmd --to pdf --no-browser --no-watch-inputs
+quarto render ./index.qmd --to pdf --no-browser --no-watch-inputs
 git add .  
 git commit -m "$1"
 git push 
