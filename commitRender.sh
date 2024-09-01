e3984add5e6cbf42ea82a01466bee69f5374827f
Krishna Neupane <dataneupane@gmail.com>
2024-09-01 08:06:06 -0400

    avramov2009dispersion

--- commitRender.sh
+++ commitRender.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git add .  
+git add .
 git commit -m "$1"
 git push
-quarto preview ./index.qmd --to pdf --no-browser --no-watch-inputs
+quarto preview /Users/krishnaneupane/Documents/personal/academic/financialanomalies/index.qmd --to pdf --no-browser --no-watch-inputs
