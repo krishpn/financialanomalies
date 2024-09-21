b76f13bfd6a30a26be4957e37dbe2790580d925d
Krishna Neupane <dataneupane@gmail.com>
2024-09-21 13:50:02 -0400

    mcconnell1984trading

--- commitRender.sh
+++ commitRender.sh
@@ -3,4 +3,4 @@ clear
 git add .
 git commit -m "$1"
 git push
-quarto preview /Users/krishnaneupane/Documents/personal/academic/financialanomalies/index.qmd --to pdf --no-browser --no-watch-inputs
+quarto render /Users/krishnaneupane/Documents/personal/academic/financialanomalies/index.qmd --to pdf --no-browser --no-watch-inputs
